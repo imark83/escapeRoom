3441fa419801eac369917997f59fa0f590cd8117
Marcos Rodríguez <marcos@unizar.es>
2019-01-25 13:48:45 +0100

    html started

--- encode.c
+++ encode.c
@@ -5,10 +5,17 @@
 
 int main(int argc, char const *argv[]) {
 
+  printf("Size = %zi\n", sizeof(short int));
+
   int i;
-  char *msg = "Seguro dental";
-  if(argc > 1) msg = argv[1];
-  int password = 0xa673bfc4;
+  char *msg = "Seguro dental!!!";
+  if(argc > 1) {
+    msg = (char *) malloc (sizeof(argv[1])+1);
+    strcpy(msg,argv[1]);
+  }
+  int password = 0xa673;
+  if(argc > 2) password = (short int ) (atoi(argv[2]) & 0x0000FFFF);
+  password |= password << 16;
   size_t len = strlen(msg)+1;
   printf("len = %zi\n", len);
 
@@ -22,12 +29,16 @@ int main(int argc, char const *argv[]) {
   for(i=0; i<n; ++i) *(intMsg+i) ^= password; 
 
   printf("Ecrypted:  %s\n", (char*) intMsg);
+  printf("numbers:\n");
+  for(i=0; i<n; ++i)
+    printf("    %12i\n", *(intMsg+i));
 
   for(i=0; i<n; ++i) *(intMsg+i) ^= password; 
 
   printf("Decrypted: %s\n", (char*) intMsg);
 
-
+  if(argc > 1) free(msg);
+  free(intMsg);
 
   return 0;
 }
